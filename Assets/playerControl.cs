51efd31b52ac9d090badad25c82aee487b2a222c
bobis <josephwaizy@gmail.com>
2025-08-27 18:15:22 -0500

    Added player control

--- Assets/playerControl.cs
+++ Assets/playerControl.cs
@@ -8,40 +8,53 @@ public class playerControl : MonoBehaviour
 {
     public Vector3 playerPosion;
     public float playerSpeed;
+    public Rigidbody rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+
         if (Input.GetKey(KeyCode.W)){
             playerPosion = transform.position;
             playerPosion.x += playerSpeed;
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(playerPosion);
+            //transform.position = playerPosion;
+            //rb.AddForce(playerPosion);
+            rb.MovePosition(playerPosion);
+
         }
         if (Input.GetKey(KeyCode.S))
         {
             playerPosion = transform.position;
             playerPosion.x -= playerSpeed;
-            transform.position = playerPosion;
+            //transform.position = playerPosion;
+            //rb.AddForce(playerPosion);
+            rb.MovePosition(playerPosion);
         }
         if (Input.GetKey(KeyCode.A))
         {
             playerPosion = transform.position;
-            playerPosion.z -= playerSpeed;
+            playerPosion.z += playerSpeed;
             //transform.position = playerPosion;
-            Rigidbody rb = GetComponent<Rigidbody>();
-            rb.AddForce(playerPosion);
+            //rb.AddForce(playerPosion);
+            rb.MovePosition(playerPosion);
+
         }
         if (Input.GetKey(KeyCode.D))
         {
             playerPosion = transform.position;
             playerPosion.z -= playerSpeed;
-            transform.position = playerPosion;
+            //transform.position = playerPosion;
+            //rb.AddForce(playerPosion);
+            rb.MovePosition(playerPosion);
+
         }
     }
 }
